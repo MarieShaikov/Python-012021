d6592270386c21e8e51c87d3327e144db350889c
marieshaikovski <m.shaikovski@gmail.com>
2021-02-21 00:09:57 +0000

    Ukol1

--- Mainn.py
+++
@@ -1,4 +0,0 @@
-item = {"title": "Čajová konvička s hrnky", "price": 899, "inStock": True}
-print("Vybraný předmět je " + item["title"] + " a cena je " + str(item["price"]) + ".")
-print("Vybraný předmět je", item["title"], "a cena je", item["price"], ".")
-print(f"Vybraný předmět je {item['title']} a cena je {item['price']}.")
