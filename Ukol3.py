ef69293f304908e8e46afd432f82c66fb819a181
marieshaikovski <m.shaikovski@gmail.com>
2021-02-21 22:51:46 +0000

    Ukol3

---
+++ Ukol3.py
@@ -0,0 +1,11 @@
+#Firma eviduje volné meetingové místnosti v průběhu dne ve slovníku.
+#líč slovníku je hodina a hodnotou slovníku seznam zasedaček,
+#které jsou v té době volné. Napiš software, který se zeptá uživatele na číslo hodiny,
+# kdy chce zamluvit meeting room. Poté vypíše počet volných místností, které jsou k dispozici.
+
+volnePokoje = {
+  9: ["Amadeus", "Goya", "Vlasy"],
+  10: ["Forman", "Goya"],
+  11: [],
+  12: ["Amadeus", "Vlasy"]
+}
