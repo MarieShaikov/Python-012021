6aa84a4f84efd3a6b22b8b5a01edc3cff1019e7a
marieshaikovski <m.shaikovski@gmail.com>
2021-03-09 19:22:39 +0000

    excel table

--- Week 4/task 4.py
+++
@@ -1,3 +0,0 @@
-import wget
-wget.download(http://nove.kodim.cz/czechitas/progr2-python/python-pro-data-1/nacteni-dat/assets/nakupy.csv)
-
---
+++ main.py
@@ -0,0 +1,6 @@
+#import wget
+#wget.download(http://nove.kodim.cz/czechitas/progr2-python/python-pro-data-1/nacteni-dat/assets/nakupy.csv)
+
+import pandas
+nakupy = pandas.read_csv('nakupy.csv')
+print(nakupy)
