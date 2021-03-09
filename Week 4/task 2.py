a4387a127a72dd02a48965b406080bbef0744d25
marieshaikovski <m.shaikovski@gmail.com>
2021-03-09 19:10:05 +0000

    task1.py

---
+++ Week 4/task 2.py
@@ -0,0 +1,24 @@
+# Balík
+# Uvažuj, že navrhuješ software pro zásilkovou společnost.
+#
+# Vytvoř třídu Package, která bude mít tři atributy - address, weightInKilos a delivered.
+# První dva atributy nastav pomocí parametrů funkce __init__.
+# Parametr delivered nastav na začátku jako False.
+# Připoj ke třídě funkci deliver, která změní hodnotu parametru delivered na True.
+# Přidej funkci getInfo, která vypíše adresu, hmotnost a informaci o tom, zda byl balík již doručen.
+# Zkus si vytvořit nějaké objekty ze třídy Package a ověř, že vše funguje.
+
+class Package:
+    def __init__(self, adress, weightInKilos):
+        self.address = adress
+        self.weightInKilos = weightInKilos
+        seld.delivered = False
+
+def getInfo(self):
+    return f'adress':{self.address}, hmotnost: {self.weightInKilos}
+
+def deliver(self):
+    self.delivered == True
+    print (f'Doruceni balik na' {self.adress})print(...)
+
+..... finish!
---
+++ Week 4/task 4.py
@@ -0,0 +1,3 @@
+import wget
+wget.download(http://nove.kodim.cz/czechitas/progr2-python/python-pro-data-1/nacteni-dat/assets/nakupy.csv)
+
