e8505083d14a9e29a0dd0d14676c407412e1d6a0
marieshaikovski <m.shaikovski@gmail.com>
2021-03-16 17:50:20 +0000

    ukol 1

--- ukol 1.py
+++ ukol 1.py
@@ -1,2 +1,8 @@
 import pandas
-jobs = pandas.read_csv('../DataAnalyst.csv')
+jobs = pandas.read_csv('DataAnalyst.csv')
+print(jobs.columns)
+print(jobs.head)
+print(jobs.shape)
+print(jobs.iloc[9]) #jeden radek, : se dava jen kdyz chceme vybrany rozsah
+#Podívej se, kde budou pracovat zájemci vybraní na dvanáctou až dvacátou pozici.
+print(jobs.iloc[12:30,[1,6]])
