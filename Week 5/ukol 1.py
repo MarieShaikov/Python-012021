e0477f07f855e5559a7a764b1957454f1d7195ba
marieshaikovski <m.shaikovski@gmail.com>
2021-03-16 18:34:27 +0000

    task 2

--- Week 5/ukol 1.py
+++ Week 5/ukol 1.py
@@ -1,8 +1,9 @@
 import pandas
-jobs = pandas.read_csv('DataAnalyst.csv')
+jobs = pandas.read_csv('../DataAnalyst.csv')
 print(jobs.columns)
 print(jobs.head)
 print(jobs.shape)
 print(jobs.iloc[9]) #jeden radek, : se dava jen kdyz chceme vybrany rozsah
+
 #Podívej se, kde budou pracovat zájemci vybraní na dvanáctou až dvacátou pozici.
-print(jobs.iloc[12:30,[1,6]])
+print(jobs.iloc[12:21,[1,6]]) #iloc is number, range between rows or columns
