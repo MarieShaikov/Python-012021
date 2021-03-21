5f3df899cddc073260e00ac4797eaa7bd1d138e6
marieshaikovski <m.shaikovski@gmail.com>
2021-03-21 15:38:17 +0000

    ukol14

--- Week 3/Ukol14.py
+++ Week 3/Ukol14.py
@@ -2,14 +2,27 @@
 # Uvažujme nyní opět třídu Employee, kterou jsme si ukázali v lekci.
 # Pro zjednodušení nyní nebudeme pracovat s dovolenou, proto nám stačí třída, která je v ukázce níže.
 #
-# class Employee:
-#   def get_info(self):
-#     return f"{self.name} pracuje na pozici {self.position}."
-#   def __init__(self, name, position):
-#     self.name = name
-#     self.position = position
+class Employee:
+
+    def __init__(self, name, position, salary, children):
+        self.name = name
+        self.position = position
+        self.salary = salary
+        self.children = children
+
+    def get_info(self):
+        return f"{self.name} works as {self.position}."
+
+    def get_tax(self):
+        return f"{salary} * 0.15 - ({children} * 1500)."
+
+
+c = Employee("George", "teacher", 50_000, 2)
+print(c.get_info, c.get_tax)
+
 # Nyní se budeme zabývat platem. Přidej třídě atribut salary (výše hrubého platu) a
 # children (počet dětí), jehož výši nastavíš ve funkci __init__().
+
 # Dále přidej funkci get_net_salary(), která vypočte a vrátí výši čisté mzdy.
 # Uvažuj zjednodušený výpočet: sazba daně je 15 % a sleva na jedno dítě 1500 Kč. Vzorec pro výpočet daně
 # tedy je: daň = hrubá mzda * 0.15 - počet dětí * 1500. Funkce vrátí výši čisté mzdy, tj. čistá mzda = hrubá mzda - daň.
