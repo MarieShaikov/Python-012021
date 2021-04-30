2647ce1f09c3df2f53948610a1bb86f2b0984819
marieshaikovski <m.shaikovski@gmail.com>
2021-04-30 19:35:33 +0100

    Ukol22.py

--- Week 5/Ukol22.py
+++ Week 5/Ukol22.py
@@ -3,15 +3,29 @@
 #
 # Stažení souboru pomocí wget:
 #
-# import wget
-# wget.download("https://raw.githubusercontent.com/pesikj/python-012021/master/zadani/5/character-deaths.csv")
+#"https://raw.githubusercontent.com/pesikj/python-012021/master/zadani/5/character-deaths.csv"
 # Pozn. Úkoly se týkají zcela nevýznamných postav, proto je riziko spoileru minimální :-)
-#
-# Načti soubor do tabulky (DataFrame) a nastav sloupec Name jako index.
-# Zobraz si sloupce, které tabulka má. Posledních pět sloupců tvoří zkratky názvů knih a informace o tom,
-# jestli se v knize postava vyskytuje.
+
+# Načti soubor do tabulky (DataFrame) a nastav sloupec Name jako index. # Zobraz si sloupce, které tabulka má. Posledních pět sloupců tvoří zkratky názvů knih a informace o tom,
+# # jestli se v knize postava vyskytuje.
+
+import pandas
+df = pandas.read_csv('Ukol22_data.csv')
+df = df.set_index("Name") # name as index
+print(df.head)
+
 # Použij funkci loc ke zjištění informací o smrti postavy jménem "Hali".
+print(df.loc["Hali", "Death Year"]) # Death Year  298.0
+
 # Použij funkci loc k zobrazení řádků mezi "Gevin Harlaw" a "Gillam".
+
+print(df.loc["Gevin Harlaw":"Gillam"])
+
 # Použij funkci loc k zobrazení řádků mezi "Gevin Harlaw" a "Gillam" a sloupce Death Year.
+
+print(df.loc["Gevin Harlaw":"Gillam", "Death Year"])
+
 # Použij funkci loc k zobrazení řádků mezi "Gevin Harlaw" a "Gillam" a informace o tom, v
 # jakých knihách se postava vyskytuje, tj. vypiš všechny sloupce mezi GoT a DwD.
+
+print(df.loc["Gevin Harlaw":"Gillam", "GoT":"DwD"])
