10756af955e4688347bacdf5a63f4a9d27e38fe9
marieshaikovski <m.shaikovski@gmail.com>
2021-05-02 00:01:32 +0100

    Ukol24.py

--- Week 5/Ukol24.py
+++ Week 5/Ukol24.py
@@ -1,19 +1,40 @@
 # Stáhni si soubor temperature.csv, který obsahuje informace o průměrné teplotě v různých městech v listopadu 2017.
 #
-# import wget
-# wget.download("https://raw.githubusercontent.com/pesikj/python-012021/master/zadani/5/temperature.csv")
+#("https://raw.githubusercontent.com/pesikj/python-012021/master/zadani/5/temperature.csv")
 # Vypiš si prvních několik řádků, ať si prohlédneš strukturu tabulky.
-#
+
+import pandas
+df = pandas.read_csv('Ukol24_data.csv')
+print(df.iloc[:4])
+
 # Dále napiš následující dotazy:
 #
 # Dotaz na měření, která byla provedena v Praze. Je na datech něco zvláštního? Napadá tě, čím to může být?
-# Zde je nápověda.
+
+task1 = df[df["City"] == "Prague"]
+print(task1)
+#it seems that the values are not in Celsia but in fahrenheit
+
 # Dotaz na měření, ve kterých je teplota (sloupec AvgTemperature) vyšší než 80 stupňů.
+
+task2 = df[df["AvgTemperature"] > 80]
+print(task2)
+
 # Dotaz na měření, ve kterých je teplota vyšší než 60 stupňů a současně bylo měření provedeno v regionu (sloupec Region)
 # Evropa (Europe).
+
+task3 = df[(df["AvgTemperature"] > 60) & (df["Region"] == "Europe")]
+print(task3)
+
 # Dotaz na extrémní hodnoty, tj. měření, ve kterých je teplota vyšší než 80 stupňů nebo menší než - 20 stupňů.
+
+extreme_values = df[(df["AvgTemperature"] > 80) | (df["AvgTemperature"] < -20)]
+print(extreme_values)
+#there are actually no avgTemper. below - 20
+
+
 # Pokročilá varianta
-#
+
 # Nainstaluj si modul pytemperature a zkus si vytvořit nový sloupec, který bude obsahovat průměrnou templotu ve
 # stupních Celsia. Ve svém programu nejprve proveď import modulu pytemperature. Nový sloupec pak přidáš do
 # tabulky tak, že nalevo od = vložíš tabulku a název nového sloupce do hranatých závorek.
